eeeb177866b0b294916006492b8264f6aa66bdf9
quankane <quanducbui2017@gmail.com>
2025-10-29 21:27:14 +0700

    auth

--- Fortis/src/api/auth.js
+++ Fortis/src/api/auth.js
@@ -1,102 +1,145 @@
-import { request, requestWithToken } from "@/utils/axios/axios-http";
+import { request } from "@/utils/axios/axios-http";
 import { axiosPrivate, axiosPublic } from "@/utils/axios/axiosInstance";
 import { getCookie, removeAllCookies, setCookie } from "@/utils/cookies";
 
 export const register = async (data) => {
-  const { username, password, firstName, lastName, email } = data;
+    const { username, password, firstName, lastName, email } = data;
 
-  try {
-    const response = await request(axiosPublic, {
-      method: "POST",
-      url: "/auth/register",
-      data: {
-        username,
-        password,
-        firstName,
-        lastName,
-        email,
-      },
-    });
+    try {
+        const response = await request(axiosPublic, {
+            method: "POST",
+            url: "/auth/register",
+            data: {
+                username,
+                password,
+                firstName,
+                lastName,
+                email,
+            },
+        });
 
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 };
 
-export const verifyOTPRegister = async (data) => {
-  const { email, otp } = data;
-  try {
-    const response = await request(axiosPublic, {
-      method: "POST",
-      url: "/auth/verify-otp",
-      data: {
-        email,
-        otp,
-      },
-    });
+export const verifyOTP = async (data) => {
+    const { email, otp } = data;
+    try {
+        const response = await request(axiosPublic, {
+            method: "POST",
+            url: "/auth/verify-otp",
+            data: {
+                email,
+                otp,
+            },
+        });
 
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 };
 
 export const login = async (data) => {
-  const { email, password } = data;
-  try {
-    const response = await request(axiosPublic, {
-      method: "POST",
-      url: "/auth/login",
-      data: {
-        email,
-        password,
-      },
-    });
+    const { email, password } = data;
+    try {
+        const response = await request(axiosPublic, {
+            method: "POST",
+            url: "/auth/login",
+            data: {
+                email,
+                password,
+            },
+        });
 
-    const { accessToken, refreshToken } = response.data.data;
-    setCookie("accessToken", accessToken);
-    setCookie("refreshToken", refreshToken);
+        const { accessToken, refreshToken, role } = response.data.data;
+        setCookie("accessToken", accessToken);
+        setCookie("refreshToken", refreshToken);
+        setCookie("role", role);
 
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 };
 
 export const logout = async () => {
-  const token = getCookie("accessToken");
-  try {
-    const response = await requestWithToken(axiosPrivate, {
-      method: "POST",
-      url: "/auth/logout",
-      data: {
-        token,
-      },
-    });
-    removeAllCookies();
-    return response.data;
-  } catch (error) {
-    console.log(error);
-  }
+    const token = getCookie("accessToken");
+    try {
+        const response = await request(axiosPrivate, {
+            method: "POST",
+            url: "/auth/logout",
+            data: {
+                token,
+            },
+        });
+        removeAllCookies();
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
 };
 
 export const refreshToken = async () => {
-  const refreshToken = getCookie("refreshToken");
-  try {
-    const response = await request(axiosPublic, {
-      method: "POST",
-      url: "/auth/refresh",
-      data: {
-        refreshToken
-      },
-    });
-    const { accessToken, refreshToken: newRefreshToken } = response.data.data;
-    setCookie("accessToken", accessToken);
-    setCookie("refreshToken", newRefreshToken);
-    return response.data;
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
+    const refreshToken = getCookie("refreshToken");
+    try {
+        const response = await request(axiosPublic, {
+            method: "POST",
+            url: "/auth/refresh",
+            data: {
+                refreshToken,
+            },
+        });
+        const { accessToken, refreshToken: newRefreshToken } =
+            response.data.data;
+        setCookie("accessToken", accessToken);
+        setCookie("refreshToken", newRefreshToken);
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export const sentEmailForgotPassword = async (data) => {
+    const { email } = data;
+    try {
+        const response = await request(axiosPublic, {
+            method: "POST",
+            url: "/auth/forgot-password",
+            data: {
+                email,
+            },
+        });
+
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export const verifyOTPForForgotPassword = async (data) => {
+    const { email, otp } = data;
+    try {
+        const response = await request(axiosPublic, {
+            method: "POST",
+            url: "/auth/verify-otp-to-reset-password",
+            data: {
+                email,
+                otp,
+            },
+        });
+
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
