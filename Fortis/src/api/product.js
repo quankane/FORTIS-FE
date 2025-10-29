cd6280b56925d561b6bea5dd5c3c6c29cd666d5c
quankane <quanducbui2017@gmail.com>
2025-10-29 21:28:04 +0700

    product

---
+++ Fortis/src/api/product.js
@@ -0,0 +1,171 @@
+import { request } from "@/utils/axios/axios-http";
+import { axiosPrivate, axiosPublic } from "@/utils/axios/axiosInstance";
+
+export const getAllProducts = async (data) => {
+    try {
+        const {
+            pageNum,
+            pageSize,
+            sortBy = "asc",
+            keyword,
+            priceRange,
+            color,
+            categoryId,
+        } = data;
+        const response = await request(axiosPublic, {
+            method: "GET",
+            url: "/product/filter",
+            params: {
+                pageNum,
+                pageSize,
+                sortBy,
+                search: [
+                    keyword && `keyword:${keyword}`,
+                    categoryId && `categoryId:${categoryId}`,
+                    priceRange && `priceRange:${priceRange}`,
+                    color && `color:${color}`,
+                ]
+                    .filter(Boolean)
+                    .join(","),
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export const createProduct = async (data) => {
+    try {
+        const {
+            productName,
+            price,
+            description,
+            detailDescription,
+            categories,
+            images,
+        } = data;
+        const requestData = {
+            productName,
+            price,
+            description,
+            detailDescription,
+            categories,
+        };
+        const formData = new FormData();
+        formData.append("request", JSON.stringify(requestData));
+        images.forEach((image) => {
+            formData.append("images", image);
+        });
+
+        const response = await request(axiosPrivate, {
+            method: "POST",
+            url: "/product",
+            data: formData,
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export const updateProduct = async (data) => {
+    try {
+        const {
+            id,
+            productName,
+            price,
+            description,
+            detailDescription,
+            categories,
+            imageIdsToDelete,
+            images,
+        } = data;
+
+        const requestData = {
+            productName,
+            price,
+            description,
+            detailDescription,
+            categories,
+            imageIdsToDelete,
+        };
+        const formData = new FormData();
+        formData.append("request", JSON.stringify(requestData));
+        images.forEach((image) => {
+            formData.append("images", image);
+        });
+        const response = await request(axiosPrivate, {
+            method: "PUT",
+            url: `/product/${id}`,
+            data: formData,
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export const getProductById = async (id) => {
+    try {
+        const response = await request(axiosPublic, {
+            method: "GET",
+            url: `/product/${id}`,
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export const deleteProduct = async (id) => {
+    try {
+        const response = await request(axiosPrivate, {
+            method: "DELETE",
+            url: `/product/${id}`,
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
+
+export const getProductByCategoryId = async (data) => {
+    try {
+        const {
+            categoryId,
+            pageNum,
+            pageSize,
+            sortBy = "asc",
+            keyword,
+            priceRange,
+            color,
+        } = data;
+        const response = await request(axiosPublic, {
+            method: "GET",
+            url: `/product/category-id/${categoryId}`,
+            params: {
+                categoryId,
+                pageNum,
+                pageSize,
+                sortBy,
+                search: [
+                    keyword && `keyword:${keyword}`,
+                    priceRange && `priceRange:${priceRange}`,
+                    color && `color:${color}`,
+                ]
+                    .filter(Boolean)
+                    .join(","),
+            },
+        });
+        return response.data;
+    } catch (error) {
+        console.log(error);
+        throw error;
+    }
+};
