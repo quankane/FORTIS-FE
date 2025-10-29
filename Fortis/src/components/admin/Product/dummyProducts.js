8b7b20cd16127c237385b475afdb8dd3e1db116f
quankane <quanducbui2017@gmail.com>
2025-10-29 21:43:23 +0700

    dummy products

---
+++ Fortis/src/components/admin/Product/dummyProducts.js
@@ -0,0 +1,278 @@
+export const dummyProducts = [
+    {
+        id: 1,
+        code: "SP001",
+        name: "Ghế có tay vịn BONHOLMEN",
+        category: "Kệ lưu trữ & tủ đầu giường",
+        image: [
+            "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4.jpg?v=1746439987253",
+        ],
+        shortDesc: "Ghế ngoài trời chắc chắn",
+        detailDesc: `<p>
+         <b>Ghế có tay vịn BONDHOLMEN</b> từ IKEA mang đến sự kết hợp hoàn hảo giữa
+        <i>phong cách hiện đại</i> và sự thoải mái tối ưu cho không gian ngoài trời của bạn.
+      </p>
+      <p>
+        Với thiết kế độc đáo, khung nhôm chắc chắn được sơn tĩnh điện màu be trang nhã
+        và mặt ngồi, lưng tựa được đan bằng các sợi polyester bền bỉ.
+      </p>
+      <ul>
+        <li>Chiều rộng: 61 cm</li>
+        <li>Chiều sâu: 69 cm</li>
+        <li>Chiều cao: 81 cm</li>
+        <li>Chiều rộng mặt ngồi: 48 cm</li>
+        <li>Chiều sâu mặt ngồi: 50 cm</li>
+        <li>Chiều cao mặt ngồi: 42 cm</li>
+        <li>Chiều cao tay vịn: 63 cm</li>
+      </ul>
+      `,
+        price: 3500000,
+        stock: 11,
+        variants: [
+            {
+                id: 101,
+                color: "Be",
+                price: 3500000,
+                stock: 5,
+                image: "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4.jpg?v=1746439987253",
+            },
+            {
+                id: 102,
+                color: "Đen",
+                price: 3600000,
+                stock: 6,
+                image: "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp18.jpg?v=1746437935323",
+            },
+        ],
+    },
+    {
+        id: 2,
+        code: "SP002",
+        name: "Ghế có tay vịn TARNO",
+        category: "Kệ lưu trữ & tủ đầu giường",
+        image: [
+            "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp18.jpg?v=1746437935323",
+        ],
+        shortDesc: "Ghế gỗ gấp tiện lợi",
+        detailDesc: `<p>
+         <b>Ghế có tay vịn BONDHOLMEN</b> từ IKEA mang đến sự kết hợp hoàn hảo giữa
+        <i>phong cách hiện đại</i> và sự thoải mái tối ưu cho không gian ngoài trời của bạn.
+      </p>
+      <p>
+        Với thiết kế độc đáo, khung nhôm chắc chắn được sơn tĩnh điện màu be trang nhã
+        và mặt ngồi, lưng tựa được đan bằng các sợi polyester bền bỉ.
+      </p>
+      <ul>
+        <li>Chiều rộng: 61 cm</li>
+        <li>Chiều sâu: 69 cm</li>
+        <li>Chiều cao: 81 cm</li>
+        <li>Chiều rộng mặt ngồi: 48 cm</li>
+        <li>Chiều sâu mặt ngồi: 50 cm</li>
+        <li>Chiều cao mặt ngồi: 42 cm</li>
+        <li>Chiều cao tay vịn: 63 cm</li>
+      </ul>
+      `,
+        price: 1000000,
+        stock: 12,
+        variants: [
+            {
+                id: 101,
+                color: "Be",
+                price: 3500000,
+                stock: 5,
+                image: "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4.jpg?v=1746439987253",
+            },
+            {
+                id: 102,
+                color: "Đen",
+                price: 3600000,
+                stock: 6,
+                image: "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4-black.jpg?v=1746439987253",
+            },
+        ],
+    },
+    {
+        id: 3,
+        code: "SP003",
+        name: "Ghế thư giãn tay vịn",
+        category: "Kệ lưu trữ & tủ đầu giường",
+        image: [
+            "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp17.jpg?v=1746437602340",
+        ],
+        shortDesc: "Ghế thư giãn thoải mái",
+        detailDesc: `<p>
+         <b>Ghế có tay vịn BONDHOLMEN</b> từ IKEA mang đến sự kết hợp hoàn hảo giữa
+        <i>phong cách hiện đại</i> và sự thoải mái tối ưu cho không gian ngoài trời của bạn.
+      </p>
+      <p>
+        Với thiết kế độc đáo, khung nhôm chắc chắn được sơn tĩnh điện màu be trang nhã
+        và mặt ngồi, lưng tựa được đan bằng các sợi polyester bền bỉ.
+      </p>
+      <ul>
+        <li>Chiều rộng: 61 cm</li>
+        <li>Chiều sâu: 69 cm</li>
+        <li>Chiều cao: 81 cm</li>
+        <li>Chiều rộng mặt ngồi: 48 cm</li>
+        <li>Chiều sâu mặt ngồi: 50 cm</li>
+        <li>Chiều cao mặt ngồi: 42 cm</li>
+        <li>Chiều cao tay vịn: 63 cm</li>
+      </ul>
+      `,
+        price: 2500000,
+        stock: 0,
+        variants: [
+            {
+                id: 101,
+                color: "Be",
+                price: 3500000,
+                stock: 5,
+                image: "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4.jpg?v=1746439987253",
+            },
+            {
+                id: 102,
+                color: "Đen",
+                price: 3600000,
+                stock: 6,
+                image: "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4-black.jpg?v=1746439987253",
+            },
+        ],
+    },
+    {
+        id: 4,
+        code: "SP004",
+        name: "Ghế có tay vịn BONHOLMEN",
+        category: "Kệ lưu trữ & tủ đầu giường",
+        image: [
+            "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4.jpg?v=1746439987253",
+        ],
+        shortDesc: "Ghế ngoài trời chắc chắn",
+        detailDesc: `<p>
+         <b>Ghế có tay vịn BONDHOLMEN</b> từ IKEA mang đến sự kết hợp hoàn hảo giữa
+        <i>phong cách hiện đại</i> và sự thoải mái tối ưu cho không gian ngoài trời của bạn.
+      </p>
+      <p>
+        Với thiết kế độc đáo, khung nhôm chắc chắn được sơn tĩnh điện màu be trang nhã
+        và mặt ngồi, lưng tựa được đan bằng các sợi polyester bền bỉ.
+      </p>
+      <ul>
+        <li>Chiều rộng: 61 cm</li>
+        <li>Chiều sâu: 69 cm</li>
+        <li>Chiều cao: 81 cm</li>
+        <li>Chiều rộng mặt ngồi: 48 cm</li>
+        <li>Chiều sâu mặt ngồi: 50 cm</li>
+        <li>Chiều cao mặt ngồi: 42 cm</li>
+        <li>Chiều cao tay vịn: 63 cm</li>
+      </ul>
+      `,
+        price: 3500000,
+        stock: 11,
+        variants: [
+            {
+                id: 101,
+                color: "Be",
+                price: 3500000,
+                stock: 5,
+                image: "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4.jpg?v=1746439987253",
+            },
+            {
+                id: 102,
+                color: "Đen",
+                price: 3600000,
+                stock: 6,
+                image: "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4-black.jpg?v=1746439987253",
+            },
+        ],
+    },
+    {
+        id: 5,
+        code: "SP005",
+        name: "Ghế có tay vịn TARNO",
+        category: "Kệ lưu trữ & tủ đầu giường",
+        image: [
+            "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp18.jpg?v=1746437935323",
+        ],
+        shortDesc: "Ghế gỗ gấp tiện lợi",
+        detailDesc: `<p>
+         <b>Ghế có tay vịn BONDHOLMEN</b> từ IKEA mang đến sự kết hợp hoàn hảo giữa
+        <i>phong cách hiện đại</i> và sự thoải mái tối ưu cho không gian ngoài trời của bạn.
+      </p>
+      <p>
+        Với thiết kế độc đáo, khung nhôm chắc chắn được sơn tĩnh điện màu be trang nhã
+        và mặt ngồi, lưng tựa được đan bằng các sợi polyester bền bỉ.
+      </p>
+      <ul>
+        <li>Chiều rộng: 61 cm</li>
+        <li>Chiều sâu: 69 cm</li>
+        <li>Chiều cao: 81 cm</li>
+        <li>Chiều rộng mặt ngồi: 48 cm</li>
+        <li>Chiều sâu mặt ngồi: 50 cm</li>
+        <li>Chiều cao mặt ngồi: 42 cm</li>
+        <li>Chiều cao tay vịn: 63 cm</li>
+      </ul>
+      `,
+        price: 1000000,
+        stock: 12,
+        variants: [
+            {
+                id: 101,
+                color: "Be",
+                price: 3500000,
+                stock: 5,
+                image: "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4.jpg?v=1746439987253",
+            },
+            {
+                id: 102,
+                color: "Đen",
+                price: 3600000,
+                stock: 6,
+                image: "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4-black.jpg?v=1746439987253",
+            },
+        ],
+    },
+    {
+        id: 6,
+        code: "SP006",
+        name: "Ghế thư giãn tay vịn",
+        category: "Giường ngủ",
+        image: [
+            "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp17.jpg?v=1746437602340",
+        ],
+        shortDesc: "Ghế thư giãn thoải mái",
+        detailDesc: `<p>
+         <b>Ghế có tay vịn BONDHOLMEN</b> từ IKEA mang đến sự kết hợp hoàn hảo giữa
+        <i>phong cách hiện đại</i> và sự thoải mái tối ưu cho không gian ngoài trời của bạn.
+      </p>
+      <p>
+        Với thiết kế độc đáo, khung nhôm chắc chắn được sơn tĩnh điện màu be trang nhã
+        và mặt ngồi, lưng tựa được đan bằng các sợi polyester bền bỉ.
+      </p>
+      <ul>
+        <li>Chiều rộng: 61 cm</li>
+        <li>Chiều sâu: 69 cm</li>
+        <li>Chiều cao: 81 cm</li>
+        <li>Chiều rộng mặt ngồi: 48 cm</li>
+        <li>Chiều sâu mặt ngồi: 50 cm</li>
+        <li>Chiều cao mặt ngồi: 42 cm</li>
+        <li>Chiều cao tay vịn: 63 cm</li>
+      </ul>
+      `,
+        price: 2500000,
+        stock: 0,
+        variants: [
+            {
+                id: 101,
+                color: "Be",
+                price: 3500000,
+                stock: 5,
+                image: "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4.jpg?v=1746439987253",
+            },
+            {
+                id: 102,
+                color: "Đen",
+                price: 3600000,
+                stock: 6,
+                image: "https://bizweb.dktcdn.net/thumb/large/100/570/902/products/sp20-4-black.jpg?v=1746439987253",
+            },
+        ],
+    },
+];
